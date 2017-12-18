6aaa675ec865da97614cc4b3fe102a2086e1d1db
Tiago Pariz <tiagopariz@gmail.com>
2017-12-18 00:01:17 -0200

    Classe base de entity

---
+++ src/DesignByContract.Domain.Core/Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DesignByContract.Domain.Core.Interfaces.Entities;
+
+namespace DesignByContract.Domain.Core.Entities
+{
+    public abstract class Entity : IEntity
+    {
+        protected Entity(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; }
+    }
+}
