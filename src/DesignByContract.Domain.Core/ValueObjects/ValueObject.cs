40c41a0fb0a344eaeaf73b21458e27f9983cf099
Tiago Pariz <tiagopariz@gmail.com>
2017-12-18 00:03:44 -0200

    Classe base de value object

---
+++ src/DesignByContract.Domain.Core/ValueObjects/ValueObject.cs
@@ -0,0 +1,8 @@
+﻿using DesignByContract.Domain.Core.Interfaces.ValueObjects;
+
+namespace DesignByContract.Domain.Core.ValueObjects
+{
+    public abstract class ValueObject : IValueObject
+    {
+    }
+}
