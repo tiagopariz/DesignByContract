a1b5ae07bb02d9b2c21e9df3cb7d0bc64e967551
Tiago Pariz <tiagopariz@gmail.com>
2017-12-18 10:05:57 -0200

    Entidade de pessoa

---
+++ src/DesignByContract.Domain/Entities/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using DesignByContract.Domain.Core.Entities;
+
+namespace DesignByContract.Domain.Entities
+{
+    public class Person : Entity
+    {
+        public Person(Guid id) : base(id) { }
+    }
+}
