87eb6085a4a6710c2a67c278fdb283dfea1a52a2
tiagopariz <tiagopariz@gmail.com>
2017-12-19 13:24:09 -0200

    Classe Description

---
+++ src/DesignByContract.Domain.Core/Notifications/Description.cs
@@ -0,0 +1,21 @@
+﻿using DesignByContract.Domain.Core.Interfaces.Notifications;
+
+namespace DesignByContract.Domain.Core.Notifications
+{
+    public abstract class Description : IDescription
+    {
+        public string Message { get; }
+
+        protected Description(string message, params string[] args)
+        {
+            Message = message;
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                Message = Message.Replace("{" + i + "}", args[i]);
+            }
+        }
+
+        public override string ToString() => Message;
+    }
+}
