933df678745430e92e7761e17d06f441f4dc0ddb
tiagopariz <tiagopariz@gmail.com>
2017-12-19 13:31:14 -0200

    Classe Notification

---
+++ src/DesignByContract.Domain.Core/Notifications/Notification.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DesignByContract.Domain.Core.Interfaces.Notifications;
+
+namespace DesignByContract.Domain.Core.Notifications
+{
+    public abstract class Notification : INotification
+    {
+        public IList<object> List { get; } = new List<object>();
+        public bool HasNotifications => List.Any();
+
+        public bool Includes(Description error)
+        {
+            return List.Contains(error);
+        }
+
+        public void Add(Description description)
+        {
+            List.Add(description);
+        }
+    }
+}
