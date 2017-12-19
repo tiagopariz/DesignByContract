5f65396601747abba30ca5ff50b10c2d56a6d2e3
tiagopariz <tiagopariz@gmail.com>
2017-12-19 16:12:19 -0200

    Value object base

--- src/DesignByContract.Domain.Core/ValueObjects/ValueObject.cs
+++ src/DesignByContract.Domain.Core/ValueObjects/ValueObject.cs
@@ -1,8 +1,25 @@
-﻿using DesignByContract.Domain.Core.Interfaces.ValueObjects;
+﻿using DesignByContract.Domain.Core.Errors;
+using DesignByContract.Domain.Core.Interfaces.ValueObjects;
+using DesignByContract.Domain.Core.Specifications;
 
 namespace DesignByContract.Domain.Core.ValueObjects
 {
     public abstract class ValueObject : IValueObject
     {
+        public Error Notification { get; } = new Error();
+        protected CompositeSpecification<object> ValidSpecification = null;
+
+        public virtual void Validate() { }
+
+        protected void Fail(bool condition, ErrorDescription error)
+        {
+            if (condition)
+                Notification.Add(error);
+        }
+
+        public bool IsValid()
+        {
+            return ValidSpecification?.IsSatisfiedBy(this) ?? true;
+        }
     }
 }
