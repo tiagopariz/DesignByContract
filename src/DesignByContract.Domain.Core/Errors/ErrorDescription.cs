1001731a2cf4a4938c8c08566cad89315592d198
tiagopariz <tiagopariz@gmail.com>
2017-12-19 15:04:48 -0200

    Descrição do erro

---
+++ src/DesignByContract.Domain.Core/Errors/ErrorDescription.cs
@@ -0,0 +1,16 @@
+﻿using DesignByContract.Domain.Core.Interfaces.Errors;
+using DesignByContract.Domain.Core.Notifications;
+
+namespace DesignByContract.Domain.Core.Errors
+{
+    public class ErrorDescription : Description
+    {
+        public ILevel Level { get; }
+
+        public ErrorDescription(string message, ILevel level, params string[] args)
+            : base(message, args)
+        {
+            Level = level;
+        }
+    }
+}
