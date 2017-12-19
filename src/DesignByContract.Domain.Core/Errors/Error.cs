fd1768fbe06800177c931ff04df55ca1f2492f26
tiagopariz <tiagopariz@gmail.com>
2017-12-19 15:07:50 -0200

    Classe de erro

---
+++ src/DesignByContract.Domain.Core/Errors/Error.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DesignByContract.Domain.Core.Notifications;
+
+namespace DesignByContract.Domain.Core.Errors
+{
+    public class Error : Notification
+    {
+        public IList<ErrorDescription> Errors => List.Cast<ErrorDescription>().Where(x => x.Level is Critical).ToList();
+        public IList<ErrorDescription> Warnings => List.Cast<ErrorDescription>().Where(x => x.Level is Warning).ToList();
+        public IList<ErrorDescription> Informations => List.Cast<ErrorDescription>().Where(x => x.Level is Information).ToList();
+        public bool HasErrors => List.Cast<ErrorDescription>().Any(x => x.Level is Critical);
+        public bool HasWarnings => List.Cast<ErrorDescription>().Any(x => x.Level is Warning);
+        public bool HasInformations => List.Cast<ErrorDescription>().Any(x => x.Level is Information);
+    }
+}
