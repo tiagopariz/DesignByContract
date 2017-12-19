118018eb45a6bb9dadb2354dc2925fd54624ca1a
tiagopariz <tiagopariz@gmail.com>
2017-12-19 13:28:02 -0200

    Interface para notificação

---
+++ src/DesignByContract.Domain.Core/Interfaces/Notifications/INotification.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using DesignByContract.Domain.Core.Notifications;
+
+namespace DesignByContract.Domain.Core.Interfaces.Notifications
+{
+    public interface INotification
+    {
+        IList<object> List { get; }
+        bool HasNotifications { get; }
+
+        bool Includes(Description error);
+        void Add(Description error);
+    }
+}
