5198096242a2df13c7d88bb11b9fae430ff4baeb
Tiago Pariz <tiagopariz@gmail.com>
2017-12-21 20:01:38 -0200

    Projeto de testes do domain core

---
+++ tests/DesignByContract.Domain.Core.Tests/ValueObjects/ValueObjectFake.cs
@@ -0,0 +1,13 @@
+﻿using DesignByContract.Domain.Core.ValueObjects;
+
+namespace DesignByContract.Domain.Core.Tests.ValueObjects
+{
+    public class ValueObjectFake : ValueObject
+    {
+        public ValueObjectFake(string fieldName)
+            : base(fieldName)
+        {
+            
+        }
+    }
+}
---
+++ tests/DesignByContract.Domain.Core.Tests/ValueObjects/ValueObjectTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignByContract.Domain.Core.Tests.ValueObjects
+{
+    [TestClass]
+    public class ValueObjectTests
+    {
+        [TestMethod]
+        public void ValueObject_New_When_Parse_FieldName()
+        {
+            var sut = new ValueObjectFake("fieldName");
+            Assert.AreEqual("fieldName", sut.FieldName);
+        }
+    }
+}
