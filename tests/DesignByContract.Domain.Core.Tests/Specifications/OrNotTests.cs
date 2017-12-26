6b7ad9fa1a438f92ccf4ff57e807ae5cfb8af9e7
tiagopariz <tiagopariz@gmail.com>
2017-12-26 19:07:16 -0200

    Or tests

---
+++ tests/DesignByContract.Domain.Core.Tests/Specifications/OrNotTests.cs
@@ -0,0 +1,51 @@
+﻿using DesignByContract.Domain.Core.Interfaces.Specifications;
+using DesignByContract.Domain.Core.Specifications;
+using DesignByContract.Domain.Core.Tests.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignByContract.Domain.Core.Tests.Specifications
+{
+    [TestClass]
+    public class OrNotTests
+    {
+        [TestMethod]
+        public void OrNotWhenFilterWithTwoSpecificationsReturnTrue()
+        {
+            ISpecification<Fake> isCustomer = new Expression<Fake>(x => x.Category == "Customer");
+            ISpecification<Fake> livesInNewYork = new Expression<Fake>(x => x.City == "New York");
+            var sut = isCustomer.OrNot(livesInNewYork);
+            Assert.IsTrue(sut.IsSatisfiedBy(new Fake { Category = "Partner", City = "Rio" }));
+        }
+
+        [TestMethod]
+        public void OrNotWhenFilterWithTwoSpecificationsReturnFalse()
+        {
+            ISpecification<Fake> isCustomer = new Expression<Fake>(x => x.Category == "Customer");
+            ISpecification<Fake> livesInNewYork = new Expression<Fake>(x => x.City == "New York");
+            var sut = isCustomer.OrNot(livesInNewYork);
+            Assert.IsFalse(sut.IsSatisfiedBy(new Fake { Category = "Customer", City = "Rio" }));
+        }
+
+        [TestMethod]
+        public void OrNotWhenFilterWithManySpecificationsReturnTrue()
+        {
+            ISpecification<Fake> isCustomer = new Expression<Fake>(x => x.Category == "Customer");
+            ISpecification<Fake> livesInNewYork = new Expression<Fake>(x => x.City == "New York");
+            ISpecification<Fake> isActive = new Expression<Fake>(x => x.Active);
+            var sut = isCustomer.OrNot(livesInNewYork)
+                                .OrNot(isActive);
+            Assert.IsTrue(sut.IsSatisfiedBy(new Fake { Category = "Customer", City = "Rio", Active = false }));
+        }
+
+        [TestMethod]
+        public void OrNotWhenFilterWithManySpecificationsReturnFalse()
+        {
+            ISpecification<Fake> isCustomer = new Expression<Fake>(x => x.Category == "Customer");
+            ISpecification<Fake> livesInNewYork = new Expression<Fake>(x => x.City == "New York");
+            ISpecification<Fake> isActive = new Expression<Fake>(x => x.Active);
+            var sut = isCustomer.OrNot(livesInNewYork)
+                                .OrNot(isActive);
+            Assert.IsFalse(sut.IsSatisfiedBy(new Fake { Category = "Partner", City = "Rio", Active = true }));
+        }
+    }
+}
---
+++ tests/DesignByContract.Domain.Core.Tests/Specifications/OrTests.cs
@@ -0,0 +1,51 @@
+﻿using DesignByContract.Domain.Core.Interfaces.Specifications;
+using DesignByContract.Domain.Core.Specifications;
+using DesignByContract.Domain.Core.Tests.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignByContract.Domain.Core.Tests.Specifications
+{
+    [TestClass]
+    public class OrTests
+    {
+        [TestMethod]
+        public void OrWhenFilterWithTwoSpecificationsReturnTrue()
+        {
+            ISpecification<Fake> isCustomer = new Expression<Fake>(x => x.Category == "Customer");
+            ISpecification<Fake> livesInNewYork = new Expression<Fake>(x => x.City == "New York");
+            var sut = isCustomer.Or(livesInNewYork);
+            Assert.IsTrue(sut.IsSatisfiedBy(new Fake { Category = "Customer", City = "New York" }));
+        }
+
+        [TestMethod]
+        public void OrWhenFilterWithTwoSpecificationsReturnFalse()
+        {
+            ISpecification<Fake> isCustomer = new Expression<Fake>(x => x.Category == "Customer");
+            ISpecification<Fake> livesInNewYork = new Expression<Fake>(x => x.City == "New York");
+            var sut = isCustomer.Or(livesInNewYork);
+            Assert.IsFalse(sut.IsSatisfiedBy(new Fake { Category = "Partner", City = "Rio"}));
+        }
+
+        [TestMethod]
+        public void OrWhenFilterWithManySpecificationsReturnTrue()
+        {
+            ISpecification<Fake> isCustomer = new Expression<Fake>(x => x.Category == "Customer");
+            ISpecification<Fake> livesInNewYork = new Expression<Fake>(x => x.City == "New York");
+            ISpecification<Fake> isActive = new Expression<Fake>(x => x.Active);
+            var sut = isCustomer.Or(livesInNewYork)
+                                .Or(isActive);
+            Assert.IsTrue(sut.IsSatisfiedBy(new Fake { Category = "Customer", City = "New York", Active = true }));
+        }
+
+        [TestMethod]
+        public void OrWhenFilterWithManySpecificationsReturnFalse()
+        {
+            ISpecification<Fake> isCustomer = new Expression<Fake>(x => x.Category == "Customer");
+            ISpecification<Fake> livesInNewYork = new Expression<Fake>(x => x.City == "New York");
+            ISpecification<Fake> isActive = new Expression<Fake>(x => x.Active);
+            var sut = isCustomer.Or(livesInNewYork)
+                                .Or(isActive);
+            Assert.IsFalse(sut.IsSatisfiedBy(new Fake { Category = "Partner", City = "Rio", Active = false }));
+        }
+    }
+}
