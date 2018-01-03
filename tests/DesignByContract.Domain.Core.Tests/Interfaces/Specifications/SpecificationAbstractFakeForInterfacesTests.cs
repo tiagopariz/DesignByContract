411281926032d91f5a3de61131264eda7561ada0
tiagopariz <tiagopariz@gmail.com>
2018-01-03 16:21:23 -0200

    Specification interfaces tests

---
+++ tests/DesignByContract.Domain.Core.Tests/Interfaces/Specifications/SpecificationAbstractFakeForInterfacesTests.cs
@@ -0,0 +1,63 @@
+﻿using DesignByContract.Domain.Core.Interfaces.Specifications;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignByContract.Domain.Core.Tests.Interfaces.Specifications
+{
+    [TestClass]
+    public abstract class SpecificationAbstractFakeForInterfacesTests
+    {
+        public abstract ISpecification<object> GetISpecificationInstance();
+
+        [TestMethod]
+        public void SpecificationParseObjectReturnIsSatisfiedByTrue()
+        {
+            var specification = GetISpecificationInstance();
+            Assert.IsTrue(specification.IsSatisfiedBy(new object()));
+        }
+
+        [TestMethod]
+        public void SpecificationAndReturnIsSatisfiedByTrue()
+        {
+            var specification1 = GetISpecificationInstance();
+            var specification2 = GetISpecificationInstance();
+            var specification3 = specification1.And(specification2);
+            Assert.IsTrue(specification3.IsSatisfiedBy(new object()));
+        }
+
+        [TestMethod]
+        public void SpecificationAndNotReturnIsSatisfiedByTrue()
+        {
+            var specification1 = GetISpecificationInstance();
+            var specification2 = GetISpecificationInstance();
+            var specification3 = specification1.AndNot(specification2);
+            Assert.IsTrue(!specification3.IsSatisfiedBy(new object()));
+        }
+
+        [TestMethod]
+        public void SpecificationOrReturnIsSatisfiedByTrue()
+        {
+            var specification1 = GetISpecificationInstance();
+            var specification2 = GetISpecificationInstance();
+            var specification3 = specification1.Or(specification2);
+            Assert.IsTrue(specification3.IsSatisfiedBy(new object()));
+        }
+
+        [TestMethod]
+        public void SpecificationOrNotReturnIsSatisfiedByTrue()
+        {
+            var specification1 = GetISpecificationInstance();
+            var specification2 = GetISpecificationInstance();
+            var specification3 = specification1.OrNot(specification2);
+            Assert.IsTrue(!specification3.IsSatisfiedBy(new object()));
+        }
+
+        [TestMethod]
+        public void SpecificationNotReturnIsSatisfiedByTrue()
+        {
+            var specification1 = GetISpecificationInstance();
+            var specification2 = GetISpecificationInstance();
+            var specification3 = specification1.Not(specification2);
+            Assert.IsTrue(!specification3.IsSatisfiedBy(new object()));
+        }
+    }
+}
---
+++ tests/DesignByContract.Domain.Core.Tests/Interfaces/Specifications/SpecificationFakeForInterfacesTests.cs
@@ -0,0 +1,15 @@
+﻿using DesignByContract.Domain.Core.Interfaces.Specifications;
+using DesignByContract.Domain.Core.Tests.Mocks.DomainCoreFake.Specifications;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignByContract.Domain.Core.Tests.Interfaces.Specifications
+{
+    [TestClass]
+    public class SpecificationFakeForInterfacesTests : SpecificationAbstractFakeForInterfacesTests
+    {
+        public override ISpecification<object> GetISpecificationInstance()
+        {
+            return new SpecificationFakeForInterfaces<object>();
+        }
+    }
+}
