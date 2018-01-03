f003613a83eb2305514cb9d396ed2ec0cb796f1f
tiagopariz <tiagopariz@gmail.com>
2018-01-03 16:49:21 -0200

    Commands tests

---
+++ tests/DesignByContract.Domain.Core.Tests/Commands/CommandTests.cs
@@ -0,0 +1,19 @@
+﻿using DesignByContract.Domain.Core.Tests.Mocks.DomainFake.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using DesignByContract.Domain.Core.Tests.Mocks.DomainFake.Commands;
+using DesignByContract.Domain.Core.Tests.Mocks.DomainFake.Entities;
+
+namespace DesignByContract.Domain.Core.Tests.Commands
+{
+    [TestClass]
+    public class CommandTests
+    {
+        [TestMethod]
+        public void CommandNewWhenParseValidEntity()
+        {
+            var sut = new SaveEntityFake(new EntityFake(Guid.NewGuid(), new ValueObjectFake("Test")));
+            sut.Run();
+        }
+    }
+}
